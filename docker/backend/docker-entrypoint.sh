b2cd7d2e959c7d8efbb659accff603a6376ca22e
Oren Epstein <oepsteindev@gmail.com>
2026-07-17 08:41:51 -0400

    fix for db initializing too quickly and not syncing with backend on spin up

--- docker/backend/docker-entrypoint.sh
+++ docker/backend/docker-entrypoint.sh
@@ -7,6 +7,16 @@ if [ ! -f composer.json ]; then
     composer require symfony/orm-pack symfony/http-client symfony/serializer-pack symfony/validator symfony/test-pack --no-interaction
 fi
 
+if [ ! -d vendor ]; then
+    composer install --no-interaction
+fi
+
+echo "Waiting for MySQL to accept connections..."
+until (exec 3<>/dev/tcp/mysql/3306) 2>/dev/null; do
+    sleep 1
+done
+exec 3>&-
+
 echo "Running database migrations..."
 php bin/console doctrine:migrations:migrate --no-interaction
 
